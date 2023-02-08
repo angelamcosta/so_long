d996afdce3a06ec53bd6f15a53912d2972d2cc18
Angela Lima <angelamcostalima@icloud.com>
2023-02-08 17:44:48 +0000

    :heavy_plus_sign: add get_next_line
    add get_next_line to read the maps

---
+++ files/get_next_line.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 10:24:04 by anlima            #+#    #+#             */
+/*   Updated: 2023/02/08 17:22:36 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+char	*get_next_line(int fd)
+{
+	static char	line[BUFFER_SIZE + 1];
+	char		*next_line;
+	int			i;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || FOPEN_MAX < fd)
+		return (NULL);
+	next_line = ft_strjoin(0, line);
+	if (ft_clean(line) > 0)
+		return (next_line);
+	i = read(fd, line, BUFFER_SIZE);
+	if (i < 0)
+	{
+		free(next_line);
+		return (NULL);
+	}
+	while (i > 0)
+	{
+		next_line = ft_strjoin(next_line, line);
+		if (ft_clean(line) > 0)
+			break ;
+		i = read(fd, line, BUFFER_SIZE);
+	}
+	return (next_line);
+}
