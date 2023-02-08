786700b75fb8e71d867369a4795dd4fd24a0b924
Angela Lima <angelamcostalima@icloud.com>
2023-02-08 16:36:44 +0000

    :construction: wip -> add so_long.c, so_long.h

---
+++ files/so_long.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 15:15:07 by anlima            #+#    #+#             */
+/*   Updated: 2023/02/08 15:18:46 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "../mlx/mlx.h"
+# include "../mlx/mlx_int.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+#endif
