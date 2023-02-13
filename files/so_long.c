a5145897c84a1e66c6668ed7472d6cb91d90eeda
Angela Lima <angelamcostalima@icloud.com>
2023-02-13 16:31:15 +0000

    :sparkles: improves movement
    supports walking with arrows or wasd

--- files/so_long.c
+++ files/so_long.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:14:44 by anlima            #+#    #+#             */
-/*   Updated: 2023/02/13 16:08:46 by anlima           ###   ########.fr       */
+/*   Updated: 2023/02/13 16:30:13 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,25 +29,29 @@ int	events(int keycode)
 		mlx_destroy_window(create_win()->mlx, create_win()->mlx_win);
 		exit(0);
 	}
-	if (keycode == 65363)
-		create_win()->x += 32;
-	if (keycode == 65361)
-		create_win()->x -= 32;
-	if (keycode == 65364)
-		create_win()->y += 32;
-	if (keycode == 65362)
-		create_win()->y -= 32;
-	mlx_clear_window(create_win()->mlx, create_win()->mlx_win);
-	mlx_put_image_to_window(create_win()->mlx, create_win()->mlx_win, create_win()->img, create_win()->x, create_win()->y);
+	// hardcoded window values for now
+	if (keycode == 65363 || keycode == 65361 || keycode == 65364 || keycode == 65362 || keycode == 97 || keycode == 119 || keycode == 115 || keycode == 100)
+	{
+		if ((keycode == 65363 || keycode == 100) && (create_win()->x + 32 <= 448))
+			create_win()->x += 32;
+		if ((keycode == 65361 || keycode == 97) && (create_win()->x - 32 >= 0))
+			create_win()->x -= 32;
+		if ((keycode == 65364 || keycode == 115) && (create_win()->y + 32 <= 448))
+			create_win()->y += 32;
+		if ((keycode == 65362  || keycode == 119) && (create_win()->y - 32 >= 0))
+			create_win()->y -= 32;
+		mlx_clear_window(create_win()->mlx, create_win()->mlx_win);
+		mlx_put_image_to_window(create_win()->mlx, create_win()->mlx_win, create_win()->img, create_win()->x, create_win()->y);
+	}
 	return (0);
 }
 
 int	main(void)
 {
-	create_win()->x = 32;
-	create_win()->y = 32;
+	create_win()->x = 0;
+	create_win()->y = 0;
 	create_win()->mlx = mlx_init();
-	create_win()->mlx_win = mlx_new_window(create_win()->mlx, 500, 500, "cozy joguinh");
+	create_win()->mlx_win = mlx_new_window(create_win()->mlx, 480, 480, "cozy joguinh");
 	create_win()->img = mlx_xpm_file_to_image(create_win()->mlx, "./frog.xpm", &create_win()->img_width, &create_win()->img_height);
 	mlx_clear_window(create_win()->mlx, create_win()->mlx_win);
 	mlx_put_image_to_window(create_win()->mlx, create_win()->mlx_win, create_win()->img, create_win()->x, create_win()->y);
