61c50beed871b6bc59e842ec09de8c8b565654b7
Angela Lima <angelamcostalima@icloud.com>
2023-03-13 21:17:45 +0000

    ⚡️ optimize map verification

--- files/read_map/verify_map.c
+++ files/read_map/verify_map.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 16:50:44 by anlima            #+#    #+#             */
-/*   Updated: 2023/02/17 15:30:27 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/13 21:16:48 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,35 +32,45 @@ int	is_empty(int fd)
 
 int	is_valid(void)
 {
-	int		i;
-	size_t	j;
+	int	i;
+	int	j;
+	int	k;
 
 	i = 0;
-	if (create_win()->map && create_win()->map[i])
-		j = ft_strlen(create_win()->map[i++]);
+	j = ft_strlen(create_win()->map[i]);
 	while (create_win()->map && create_win()->map[i])
 	{
+		k = j - 1;
 		if (!create_win()->map[i + 1])
 			j--;
-		if (ft_strlen(create_win()->map[i++]) != j)
+		if ((int)ft_strlen(create_win()->map[i]) != j)
 			return (0);
+		while (--k >= 0)
+		{
+			if ((i == 0 || !create_win()->map[i + 1])
+				&& create_win()->map[i][k] != '1')
+				return (0);
+			if ((k == 0 || k == (j - 2)) && create_win()->map[i][k] != '1')
+				return (0);
+		}
+		i++;
 	}
 	return (1);
 }
 
-int	verify_map(char *file_name)
+int	verify_map(char *filename)
 {
 	int		fd;
 	int		cont;
 
-	fd = open(file_name, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	cont = is_empty(fd);
 	if (!cont || cont < 3)
 		return (0);
 	create_win()->map = (char **)malloc(sizeof(char *) * (cont + 1));
 	if (!create_win()->map)
 		return (0);
-	fd = open(file_name, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	cont = 0;
 	while (1)
 	{
