e002116f6ff9ec2fd5188e7bee3ff4e375e4d3da
Angela Lima <angelamcostalima@icloud.com>
2023-03-14 16:26:58 +0000

    :construction_worker: update makefile and header

--- files/so_long.h
+++ files/so_long.h
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:15:07 by anlima            #+#    #+#             */
-/*   Updated: 2023/03/13 21:30:17 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/14 16:12:06 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,35 @@ typedef struct s_win
 	void	*background;
 	int		win_width;
 	int		win_height;
+}				t_win;
+
+typedef struct s_map
+{
 	char	**map;
 	int		c;
 	int		p;
 	int		e;
-}				t_win;
+	int		other_chars;
+	int		c_count;
+	int		p_count;
+	int		e_count;
+	int		i;
+	int		j;
+}				t_map;
 
 t_win	*create_win(void);
+t_map	*create_map(void);
 int		events(int keycode);
 void	create_background(void);
-char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(char *s);
 int		ft_clean(char *str);
+char	*get_next_line(int fd);
+int		valid_chars(void);
+void	check_chars(char c, int i, int j);
 int		is_empty(int fd);
+void	check_path(int i, int j);
+int		is_valid(void);
 int		verify_map(char *file_name);
 
 #endif
